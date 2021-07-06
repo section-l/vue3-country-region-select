1ef814b677cb64dffbfdd28961f6b946a048d0df
Trouni <34345789+trouni@users.noreply.github.com>
2021-07-06 23:16:38 +0900

    Fix missing autocomplete prop

--- src/components/region-select.vue
+++ src/components/region-select.vue
@@ -12,6 +12,7 @@ export default {
     regionName: Boolean,
     className: String,
     shortCodeDropdown: Boolean,
+    autocomplete: Boolean,
     placeholder: {
       type: String,
       default: 'Select Region'
